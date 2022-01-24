9422fa58e62857de8f4919ee36376838249537d3
Sarwan <iamsarwandahri@gmail.com>
2022-01-24 19:39:54 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: Home(),
       ),
     );
+
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Random rand = Random();
+  int num = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Ask Me Anything',
+        ),
+      ),
+      body: Center(
+        child: Expanded(
+          child: FlatButton(
+            child: Image.asset('images/ball$num.png'),
+            onPressed: () {
+              setState(() {
+                num = rand.nextInt(5) + 1;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
